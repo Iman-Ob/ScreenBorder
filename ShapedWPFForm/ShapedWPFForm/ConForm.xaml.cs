4f0a097da9b367edbfc52629379dd4ca71ac61c4
Iman-Ob <83701736+Iman-Ob@users.noreply.github.com>
2024-12-29 16:36:09 +0300

    Add files via upload

---
+++ ShapedWPFForm/ShapedWPFForm/ConForm.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ShapedWPFForm
+{
+    /// <summary>
+    /// Interaction logic for ConForm.xaml
+    /// </summary>
+    public partial class ConForm : Window
+    {
+        public ConForm()
+        {
+            InitializeComponent();
+            textBox1.Text = Properties.Settings.Default.PName;
+            textBox2.Text = Properties.Settings.Default.FullPath;
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            Properties.Settings.Default.PName = textBox1.Text;
+            Properties.Settings.Default.FullPath = textBox2.Text;
+            Properties.Settings.Default.Save();
+            this.Close();
+        }
+    }
+}
---
+++ ShapedWPFForm/ShapedWPFForm/ShapedWindow.xaml.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+using System.Windows.Media.Animation;
+
+namespace ShapedWPFForm
+{
+    /// <summary>
+    /// Interaction logic for ShapedWindow.xaml
+    /// </summary>
+    public partial class ShapedWindow : Window
+    {
+       
+        public ShapedWindow()
+        {
+            InitializeComponent();
+            this.Topmost = true;
+            System.Windows.Threading.DispatcherTimer dispatcherTimer = new System.Windows.Threading.DispatcherTimer();
+            dispatcherTimer.Tick += dispatcherTimer_Tick;
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 4);
+            dispatcherTimer.Start();
+
+
+         
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            string s = @""+Properties.Settings.Default.FullPath;
+            System.Diagnostics.Process.Start(s);
+            this.btnLogin.Visibility = Visibility.Hidden;
+            this.button2.Visibility = Visibility.Hidden;
+        }
+
+        protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
+        {
+            //base.OnMouseLeftButtonDown(e);
+            //DragMove();
+        }
+
+   
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (Process Proc in Process.GetProcesses())
+                if (Proc.ProcessName.Equals(Properties.Settings.Default.PName))  //Process Excel?
+                    Proc.Kill();
+
+            System.Windows.Application.Current.Shutdown();
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            ConForm c = new ConForm();
+            c.Show();
+        }
+
+        private void dispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            ColorAnimation ca = new ColorAnimation();
+            ca.From = (Color)ColorConverter.ConvertFromString("#0066FF");
+            ca.To = (Color)ColorConverter.ConvertFromString("#FF0000");
+            ca.Duration = TimeSpan.FromSeconds(3);
+            this.border.BorderBrush.BeginAnimation(SolidColorBrush.ColorProperty, ca);
+            this.Focus();
+            this.Topmost = true;
+        }
+        private void _canExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = false;
+            e.Handled = true;
+        }
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.Focus();
+            this.Topmost = true;
+            foreach (Process Proc in Process.GetProcesses())
+                if (Proc.ProcessName.Equals("ChatiApp"))  //Process Excel?
+                    Proc.Kill();
+        }
+
+        protected override void OnPreviewKeyDown(KeyEventArgs e)
+        {
+            if (Keyboard.Modifiers == ModifierKeys.Alt && e.SystemKey == Key.F4 ||
+               Keyboard.Modifiers == ModifierKeys.Control && e.SystemKey == Key.Escape ||
+                Keyboard.Modifiers == ModifierKeys.Windows && e.SystemKey == Key.D)
+            {
+                e.Handled = false;
+            }
+            else
+            {
+                base.OnPreviewKeyDown(e);
+            }
+
+        }
+        
+    }
+}
